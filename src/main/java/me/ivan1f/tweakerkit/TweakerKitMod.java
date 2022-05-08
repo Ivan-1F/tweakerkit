4ac68d18d72fd152b8148508db8921193b73d845
Ivan1F <wyf0762@foxmail.com>
2022-05-09 04:29:13 +0800

    clean up

--- src/main/java/me/ivan1f/tweakerkit/TweakerKitMod.java
+++ src/main/java/me/ivan1f/tweakerkit/TweakerKitMod.java
@@ -1,14 +1,14 @@
-package me.ivan1f.template_mod;
+package me.ivan1f.tweakerkit;
 
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.loader.api.FabricLoader;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-public class TemplateMod implements ModInitializer {
+public class TweakerKitMod implements ModInitializer {
     public static final Logger LOGGER = LogManager.getLogger();
-    public static final String MOD_NAME = "Template Mod";
-    public static final String MOD_ID = "template_mod";
+    public static final String MOD_NAME = "TweakerKit";
+    public static final String MOD_ID = "tweakerkit";
     public static String VERSION;
 
     @Override
