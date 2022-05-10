ec23607c5b1aff03e0accb058fc28b9bfb627fad
Ivan1F <wyf0762@foxmail.com>
2022-05-10 17:38:14 +0800

    removed search bar width mixin

--- src/main/java/me/ivan1f/tweakerkit/mixins/gui/WidgetListConfigOptionsMixin.java
+++ src/main/java/me/ivan1f/tweakerkit/mixins/gui/WidgetListConfigOptionsMixin.java
@@ -14,7 +14,6 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.ModifyArg;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
@@ -31,28 +30,13 @@ public abstract class WidgetListConfigOptionsMixin extends WidgetListConfigOptio
         super(x, y, width, height, configWidth);
     }
 
+    @SuppressWarnings("BooleanMethodIsAlwaysInverted")
     private boolean isTweakerKitConfigGui() {
         return this.parent instanceof TweakerKitConfigGui;
     }
 
     private <T> T getFeatureValue(FeatureConfig.Key<T> key) {
         return ((TweakerKitConfigGui) this.parent).getFeatureValue(key);
-    }
-
-    @ModifyArg(
-            method = "<init>",
-            at = @At(
-                    value = "INVOKE",
-                    target = "Lfi/dy/masa/malilib/gui/widgets/WidgetSearchBar;<init>(IIIIILfi/dy/masa/malilib/gui/interfaces/IGuiIcon;Lfi/dy/masa/malilib/gui/LeftRight;)V"
-            ),
-            index = 2,
-            remap = false
-    )
-    private int tweakerKitSearchBarWidth(int width) {
-//        if (this.parent instanceof TweakerKitConfigGui) {
-            width -= 120;
-//        }
-        return width;
     }
 
     @ModifyVariable(
@@ -65,7 +49,8 @@ public abstract class WidgetListConfigOptionsMixin extends WidgetListConfigOptio
             remap = false
     )
     private int updateMaxNameLengthIfUsingTweakerKitOptionLabelAndShowsOriginalText$tweakerkit(int maxWidth, List<GuiConfigsBase.ConfigOptionWrapper> wrappers) {
-        if (this.parent instanceof TweakerKitConfigGui) {
+        if (!isTweakerKitConfigGui()) return maxWidth;
+        if (getFeatureValue(FeatureConfig.TRANSLATABLE_LABEL)) {
             for (GuiConfigsBase.ConfigOptionWrapper wrapper : wrappers) {
                 if (wrapper.getType() == GuiConfigsBase.ConfigOptionWrapper.Type.CONFIG) {
                     IConfigBase config = Objects.requireNonNull(wrapper.getConfig());
